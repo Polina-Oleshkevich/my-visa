acbe0f454dc51ddee035874010e9ff8964d4507c
Polina-Oleshkevich <poli3108@yandex.ru>
2022-08-27 16:42:28 +0300

    Diploma

---
+++ src/main/java/com/myvisa/myvisa/MyVisaApplication.java
@@ -0,0 +1,10 @@
+package com.myvisa.myvisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVisaApplication { public static void main(String[] args) {
+    SpringApplication.run(MyVisaApplication.class, args);
+}
+}
