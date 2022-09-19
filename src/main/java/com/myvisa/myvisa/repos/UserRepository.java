80173cd0b5da8a261d16a94e51dc2113d944facd
Polina-Oleshkevich <poli3108@yandex.ru>
2022-09-19 12:00:04 +0300

    Diploma

--- src/main/java/com/myvisa/myvisa/repos/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.myvisa.myvisa.repos;
-
-import com.myvisa.myvisa.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
---
+++ src/main/java/com/myvisa/myvisa/securingweb/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.myvisa.myvisa.securingweb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+
+    private DataSource dataSource;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/requests/**").authenticated()
+                .antMatchers("/admin/**", "/client/**", "/center/**", "/manager/**", "/visa/**").authenticated()
+                .and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
+    }
